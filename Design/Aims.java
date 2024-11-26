112210659d750bb32708d3fb62d447f0e566ff35
nguyenthanh0312 <150718172+nguyenthanh0312@users.noreply.github.com>
2024-11-26 16:19:28 +0700

    Update Aims.java

--- Design/Aims.java
+++ Design/Aims.java
@@ -1,18 +1,17 @@
 package Aims;
 import cart.cart;
 import DigitalVideoDisc.DigitalVideoDisc;
-import Store.Store;
 public class Aims {
 
 	public static void main(String[] args) {
 		cart anOrder = new cart();
-        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King","Animation",
-                "Roger Allers",87,19.95f);
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("Doraemon","Naruto",
+                "Roger Allers",90,19.95f);
         anOrder.addDigitalVideoDisc(dvd1);
-        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star wars","Science Fiction",
-                "Geogre Lucas",87,24.95f);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Ford","Lambogini",
+                "Geogre Lucas",90,24.95f);
         anOrder.addDigitalVideoDisc(dvd2);
-        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin","Animation",18.99f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Luffy","Zoro",18.99f);
         anOrder.addDigitalVideoDisc(dvd3);
         anOrder.print();
         anOrder.removeDigitalVideoDisc(dvd2);
